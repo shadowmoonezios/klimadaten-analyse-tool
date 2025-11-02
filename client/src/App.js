4010286a211508e46c395916ee1316215bdb2103
shadowmoonezios <agushifelde@hotmail.com>
2025-11-02 12:18:04 +0200

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Klimadaten Analyse Tool</h1>
+        <p>Lade Deine Klimadaten hoch, um Analysen durchzuführen.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
